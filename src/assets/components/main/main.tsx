f744fc71f494d317484a84cb3898837fb54e3fc8
ruthinunes <ruthinunesdev@gmail.com>
2023-06-09 20:57:34 -0300

    Refactoring main component

--- src/assets/components/main/main.tsx
+++ src/assets/components/main/main.tsx
@@ -1,20 +1,17 @@
-import { MdOutlineEmail } from "react-icons/md";
-import { RiUserSmileLine } from "react-icons/Ri";
 import styles from "./main.module.css";
 
 const Main = () => {
   return (
     <>
-      <main className={styles.mainCtn}>
-        <div>
-          <p>Hello, we are You X</p>
-          <h1>We enjoy designing tech websites and digital products</h1>
-          <button>Get in touch</button>
+      <main className={styles.heroCtn}>
+        <div className={styles.heroContent}>
+          <p>Hey, I'm John Carter</p>
+          <h1>I enjoy designing tech websites and digital products</h1>
+          <button type="button" className={styles.heroButton}>
+            Get in touch
+          </button>
         </div>
-
-        <div>
-            <img src="src/assets/images/heroImg.png" className={styles.img} />
-        </div>
+        <img src="src/assets/images/heroImg.png" className={styles.heroImg} />
       </main>
     </>
   );
