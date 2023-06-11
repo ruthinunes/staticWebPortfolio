94b059226198df7c67b1b252104ffb5e8363f60b
ruthinunes <ruthinunesdev@gmail.com>
2023-06-10 22:00:28 -0300

    Adding menu hamburger icons / add useState funcion and setting in components

--- src/assets/components/header/Header.tsx
+++ src/assets/components/header/Header.tsx
@@ -1,18 +1,43 @@
 import { MdOutlineEmail } from "react-icons/md";
 import { RiUserSmileLine } from "react-icons/Ri";
+import { AiOutlineMenu, AiFillCloseCircle } from "react-icons/ai";
+import { useState } from "react";
 import styles from "./Header.module.css";
 
 const Header = () => {
+  const [isActive, setIsActive] = useState(false);
+
+  const toggleMenu = () => {
+    setIsActive(!isActive);
+  };
+
   return (
     <>
       <header className={styles.headerContainer}>
         <a href="#" className={styles.logoCtn}>
-          <RiUserSmileLine size={45} />
+          <RiUserSmileLine className={styles.logoIcon} />
           <span>You</span>
           <span>X</span>
         </a>
 
-        <div className={styles.headerList}>
+        <button className={styles.menuIcon} onClick={toggleMenu}>
+          {isActive ? <AiFillCloseCircle color="white"/> : <AiOutlineMenu />}
+        </button>
+        <div
+          className={
+            isActive
+              ? styles.menuOverlay
+              : [styles.menuOverlay, styles.hide].join(" ")
+          }
+        ></div>
+
+        <nav
+          className={
+            isActive
+              ? styles.headerList
+              : [styles.headerList, styles.hide].join(" ")
+          }
+        >
           <ul className={styles.list}>
             <li>
               <a href="#">Home</a>
@@ -35,9 +60,14 @@ const Header = () => {
               </ul>
             </li>
           </ul>
-        </div>
-
-        <div className={styles.headerEmail}>
+        </nav>
+        <div
+          className={
+            isActive
+              ? styles.headerEmail
+              : [styles.headerEmail, styles.hide].join(" ")
+          }
+        >
           <MdOutlineEmail size={25} />
           <a href="#" className="email-link">
             john@you.com
